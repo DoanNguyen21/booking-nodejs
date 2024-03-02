c1dfe1fa50182e10f9e6f93cd65e1ac247d7bbc8
Doan Nguyen <gindisme21@gmail.com>
2024-03-02 20:14:00 +0700

    commit

---
+++ src/migrations/migration-create-history.js
@@ -0,0 +1,43 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    // address: DataTypes.STRING,
+    // description: DataTypes.STRING,
+    // image: DataTypes.STRING
+
+    await queryInterface.createTable('histories', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      patientId: {
+        type: Sequelize.INTEGER
+      },
+      doctorId: {
+        type: Sequelize.INTEGER
+      },
+      description: {
+        type: Sequelize.TEXT
+      },
+      files: {
+        type: Sequelize.TEXT
+      },
+     
+     
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('histories');
+  }
+};
