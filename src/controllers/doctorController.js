8c109128a635d6ab8e9653515b0839bce4f38cf4
Doan Nguyen <gindisme21@gmail.com>
2024-04-15 20:42:28 +0700

    second commit

--- src/controllers/doctorController.js
+++ src/controllers/doctorController.js
@@ -1,3 +1,4 @@
+import db from "../models";
 import doctorService from "../services/doctorService";
 
 let getTopDoctorHome = async (req, res) => {
@@ -15,6 +16,48 @@ let getTopDoctorHome = async (req, res) => {
   }
 };
 
+let getAllDoctors = async (req, res) => {
+  try {
+    let doctors = await doctorService.getAllDoctors();
+    return res.status(200).json(doctors);
+  } catch (e) {
+    console.log(e);
+    return res.status(200).json({
+      errCode: -1,
+      errMessage: "Error from server...",
+    });
+  }
+};
+
+let postInforDoctor = async (req, res) => {
+  try {
+    let response = await doctorService.saveDetailInforDoctor(req.body);
+    return res.status(200).json(response);
+  } catch (e) {
+    console.log(e);
+    return res.status(200).json({
+      errCode: -1,
+      errMessage: "Error from server...",
+    });
+  }
+};
+
+let getDetailDoctorById = async (req, res) => {
+  try {
+    let infor = await doctorService.getDetailDoctorById(req.query.id);
+    return res.status(200).json(infor);
+  } catch (e) {
+    console.log(e);
+    return res.status(200).json({
+      errCode: -1,
+      errMessage: "Error from server...",
+    });
+  }
+};
+
 module.exports = {
   getTopDoctorHome: getTopDoctorHome,
+  getAllDoctors: getAllDoctors,
+  postInforDoctor: postInforDoctor,
+  getDetailDoctorById: getDetailDoctorById,
 };
